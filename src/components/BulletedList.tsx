a6640ec686dce721e4efc46fd7168a1d1d9ba15a
leiDnedyA <aydendiel@gmail.com>
2024-02-06 18:08:03 -0500

    feat: added resume button and clickable links for socials

--- src/components/BulletedList.tsx
+++ src/components/BulletedList.tsx
@@ -1,9 +1,15 @@
+import './BulletedList.css';
 
-function BulletedList(props: {items: string[]}) {
-  return <ul>
-    {props.items.map((item, index) => {
+function BulletedList(props: {items: string[], clickable?: true}) {
+  return <ul className="BulletedList">
+    {props.clickable === true ?
+    props.items.map((item, index) => {
+        return <li key={`item_${index}`}><a href={item}>{item}</a></li>
+      }) :
+    props.items.map((item, index) => {
         return <li key={`item_${index}`}>{item}</li>
-      })}
+      })
+    }
   </ul>
 }
 
