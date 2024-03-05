81447758185bf27c30c8fa54b3792662ff3b0bb6
leiDnedyA <aydendiel@gmail.com>
2024-03-05 13:25:13 -0500

    feat: sripped site down to basic content and installed material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
+import Button from '@mui/material/Button';
+
 import CodeBlock from './components/CodeBlock.tsx';
 import BulletedList from './components/BulletedList.tsx'
-
-const CODE_TEXT = `
-/**
-*   /$$$$$$                  /$$                    
-*  /$$__  $$                | $$                    
-* | $$  \\ $$ /$$   /$$  /$$$$$$$  /$$$$$$  /$$$$$$$ 
-* | $$$$$$$$| $$  | $$ /$$__  $$ /$$__  $$| $$__  $$
-* | $$__  $$| $$  | $$| $$  | $$| $$$$$$$$| $$  \\ $$
-* | $$  | $$| $$  | $$| $$  | $$| $$_____/| $$  | $$
-* | $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$  | $$
-* |__/  |__/ \\____  $$ \\_______/ \\_______/|__/  |__/
-*            /$$  | $$                              
-*           |  $$$$$$/                              
-*            \\______/                               
-*  /$$$$$$$  /$$           /$$                      
-* | $$__  $$|__/          | $$                      
-* | $$  \\ $$ /$$  /$$$$$$ | $$                      
-* | $$  | $$| $$ /$$__  $$| $$                      
-* | $$  | $$| $$| $$$$$$$$| $$                      
-* | $$  | $$| $$| $$_____/| $$                      
-* | $$$$$$$/| $$|  $$$$$$$| $$                      
-* |_______/ |__/ \\_______/|__/                      
-*/
-`;
 
 const WORK_EXPERIENCES = [
 "Jan 2024 - Now: Coding AI Trainer & Interviewer @ Scale AI & Remotasks",
@@ -56,29 +34,28 @@ function App() {
 
   return (
     <div id="App">
-      <div className="card">
-        <h1>Hi, I'm Ayden!</h1>
-        <h2>CS Student @ UMass Boston, President of UMass Boston CS Club</h2>
-        <p>
-          Here are a brief list of my work experiences:
-        </p>
-        <BulletedList items={WORK_EXPERIENCES}/>
-        <p>
-          These are some of the technologies that I have experience with:
-        </p>
-        <BulletedList items={TECHNOLOGIES}/>
-        <p>
-          Follow me on LinkedIn & GitHub :)
-        </p>
-        <BulletedList items={SOCIAL_LINKS} clickable/>
-        <button className="resume-btn" onClick={() => {
-          const result = window.open(RESUME_URL, '_blank');
-          if (result !== null) {
-            result.focus();
-          }
-        }}>My Resume</button>
-        <CodeBlock text={CODE_TEXT} doScroll={true}/>
-      </div>
+      <h1>Hi, I'm Ayden!</h1>
+
+      <h2>CS Student @ UMass Boston, President of UMass Boston CS Club</h2>
+      <p>
+        Here are a brief list of my work experiences:
+      </p>
+      <BulletedList items={WORK_EXPERIENCES}/>
+      <p>
+        These are some of the technologies that I have experience with:
+      </p>
+      <BulletedList items={TECHNOLOGIES}/>
+      <p>
+        Follow me on LinkedIn & GitHub :)
+      </p>
+      <BulletedList items={SOCIAL_LINKS} clickable/>
+      <Button variant="constrained" onClick={() => {
+        const result = window.open(RESUME_URL, '_blank');
+        if (result !== null) {
+          result.focus();
+        }
+      }}>My Resume</Button>
+    
     </div>
   )
 }
