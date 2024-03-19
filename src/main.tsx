17e95ce51c527ea766bebbb451c08a74e88d121c
leiDnedyA <aydendiel@gmail.com>
2024-03-19 12:47:10 -0400

    feat: added dark mode theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+
+/* Font stuff */
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab, faReact } from '@fortawesome/free-brands-svg-icons';
@@ -15,9 +18,18 @@ import '@fontsource/roboto/700.css';
 
 library.add(fab, fas, faReact);
 
+/* Theme stuff */
+const theme = createTheme({
+  palette: {
+    mode: "dark"
+  }
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
