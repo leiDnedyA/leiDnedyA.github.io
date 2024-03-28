ce60ff30615d175872f62205c3a1c8f540ed6c8a
leiDnedyA <aydendiel@gmail.com>
2024-03-28 19:20:27 -0400

    fix: fixed issue with spotlight expanding page height when at bottom of screen + built

--- src/components/BackgroundSpotlight.tsx
+++ src/components/BackgroundSpotlight.tsx
@@ -1,12 +1,19 @@
 import { useCallback, useEffect, useState } from 'react';
 import './styles/BackgroundSpotlight.css'
 
+// Diagonal length of spotlight rectangle to avoid putting it below page end
+const DIAGONAL_LENGTH = Math.sqrt(Math.pow(200, 2) + Math.pow(200, 2));
+
 function BackgroundSpotlight() {
   const [position, setPosition] = useState([window.innerWidth / 2, 0]);
   const [blurHeight, setBlurHeight] = useState(window.innerWidth);
 
   const mouseMoveCallback = useCallback((event: MouseEvent) => {
-      setPosition([event.pageX, event.pageY]);
+      if (event.pageY + DIAGONAL_LENGTH < blurHeight) {
+        setPosition([event.pageX, event.pageY]);
+      } else{
+        setPosition([event.pageX, blurHeight - DIAGONAL_LENGTH]);
+      }
     }, []);
 
   const windowLoadCallback = useCallback(() => {
